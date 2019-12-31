bb1d75af9ab140368e10694620f3573902984078
Heikki Johannes Hilden <hildenjohannes@gmail.com>
2019-12-31 18:21:46 +0100

    Add favicon

--- wp-content/themes/fri-wire/header.php
+++ wp-content/themes/fri-wire/header.php
@@ -3,14 +3,13 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php wp_title(''); ?><?php if (wp_title( '', false )) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <link href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" rel="stylesheet" crossorigin="anonymous">
     <link href="//fonts.googleapis.com/css?family=Rokkitt:100,200,300,400,700&display=swap" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/icons/cropped-Favicon-512x512_FRI-32x32.png" />
     <?php wp_head(); ?>
     <script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
